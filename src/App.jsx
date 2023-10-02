76316e186b505e7e5888c8898fb65cc21136cc1c
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-10-02 18:11:03 -0400

    Adding linkedin link to footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
 
+
 const App = () => {
   const [count, setCount] = useState(0)
+  
 
   return (
     <div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
                     <ul>
                         <li>Go to Top</li>
                         <li><a href="https://keoshaug.github.io/" target='_blank'>My Portfolio</a></li>
-                        <li>Social Media Link 2</li>
+                        <li><a href="https://www.linkedin.com/in/kevinoshaughnessy/" target='_blank'>Linkedin</a></li>
                     </ul>
                 </nav>
             </footer>
