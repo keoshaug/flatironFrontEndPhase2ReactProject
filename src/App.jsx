a71c13addf2c8267bafbd078f70155b0c4691178
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-10-02 19:11:32 -0400

    adding card

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
+import { useState } from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import './App.css';
 
 
-const App = () => {
+function App() {
+
   const [count, setCount] = useState(0)
   
 
   return (
-    <div>
-    <Header />
-    <Main />
-    <Footer />
+    <div classNme="App">
+      <Header />
+      <Main />
+      <Footer />
     </div>
   )
 }
