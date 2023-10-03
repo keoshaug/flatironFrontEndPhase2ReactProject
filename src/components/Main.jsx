8f93efac85b57ca62f1e79e38e0bbdeb1969853c
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-10-02 21:34:39 -0400

    adding Card as a child of main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { ReactDOM } from "react";
 import App from "../App";
+import { Card } from "./Card";
 
 const Main = () => {
     return (
         <>
             <main>
                 <h1></h1>
+                    <div className="card">
+                        <Card/>
+                    </div>
                 {/* fetch() */}
                 {/* .then(res => res.json())
                 .then(data =>{}) */}
