a2c1fe8106344483a9089624403eab5f27cd0e88
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-10-03 13:18:43 -0400

    Adding move to top button to bottom of the page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,11 +9,12 @@ const Footer = () => {
                 <h2>Links</h2>
                 <br/>
                 <nav>
-                        <button className="ft-btn">Go to Top</button>
-                        <br/>
                         <a href="https://keoshaug.github.io/" target='_blank'>My Portfolio</a>
                         <br/>
                         <a href="https://www.linkedin.com/in/kevinoshaughnessy/" target='_blank'>Linkedin</a>
+                        <br/>
+                        <br/>
+                        <button className="ft-btn">Go to Top</button>
                 </nav>
             </footer>
         
