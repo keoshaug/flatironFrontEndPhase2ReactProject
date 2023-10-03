8f04f2bc064367dd0f09b16da24f4727e69c26b2
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-10-03 10:48:29 -0400

    designing and centering card button for remove

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import "./Card.css"
 
 const card = () => {
     return (
-        <div className="card">
-            {/* <h2>{quote}</h2>
-            <h3>{author}</h3>
-            <h3>{work}</h3>
-            <h3>{category}</h3> */}
-            <button>Remove</button>
+        <div className="card-container">
+            <h2>quote</h2>
+            <h3>author</h3>
+            <h3>work</h3>
+            <h3>category</h3>
+            <button className="card-btn">Remove</button>
         </div>
     )
 }
