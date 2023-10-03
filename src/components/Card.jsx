3860321f432300b1c7ca6bc320b0bf63567a35d3
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-10-03 10:22:08 -0400

    adding h3 tags to card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 const card = () => {
     return (
-        <div>Card</div>
+        <div className="card">
+            {/* <h2>{quote}</h2>
+            <h3>{author}</h3>
+            <h3>{work}</h3>
+            <h3>{category}</h3> */}
+        </div>
     )
 }
 
