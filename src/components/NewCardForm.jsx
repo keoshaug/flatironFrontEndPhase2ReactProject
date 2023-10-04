9047c5822cb1a8858db405e73e8db227f8669e4a
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-10-04 10:01:35 -0400

    adding onchange to each input in NewCardForm

--- src/components/NewCardForm.jsx
+++ src/components/NewCardForm.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState} from "react";
 import Card  from "./Card";
 
 function NewCardForm () {
+    const [formData, setFormData] = useState({
+        quote: "",
+        author: "",
+        work: "",
+        category: ""
+    })
+
+    function handleSubmit(e) {
+        e.preventDefault()
+    }
+
+
     return (
             <div className="new-card-form">
                 <h3>Add New Quote</h3>
                 <form name="new-card-form" id="cardForm">
-                    <input type="text" quote="quote" placeholder="Quote" />
-                    <input type="text" author="author" placeholder="Author" />
-                    <input type="text" work="work" placeholder="Work" />
-                    <button type="submit">Add Quote</button>
+                    <input type="text" quote="quote" value={formData.quote} placeholder="Quote" onChange={handleChange}/>
+                    <input type="text" author="author"  value={formData.author} placeholder="Author" onChange={handleChange}/>
+                    <input type="text" work="work"  value={formData.work} placeholder="Work" onChange={handleChange} />
+                    <input type="text" work="category"  value={formData.category} placeholder="Category" onChange={handleChange} />
+                    <button type="submit" onClick={handleSubmit}>Add Quote</button>
                 </form>
             </div>
     );
