6f00606d7e083b22e6f7115a63a4134c897b446d
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-10-04 08:39:43 -0400

    adding handleDelete with fetch method Delete to card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,6 +4,16 @@ import "./Card.css"
 function Card({quotes}) {
 
     const {id, quote, work, author, category} = quotes
+
+
+
+
+    function handleDelete () {
+        fetch(`http://localhost:4000/data${id}`, {
+          method: "DELETE"
+        })
+        .then(onDeleteClick(id))
+      }
     
 
     return (
@@ -13,7 +23,7 @@ function Card({quotes}) {
             <p>{author}</p>
             <p>{category}</p>
             <br/>
-            <button className="card-btn">Remove</button>
+            <button className="card-btn" onClick={handleDelete}>Remove</button>
         </div>
     )
 }
