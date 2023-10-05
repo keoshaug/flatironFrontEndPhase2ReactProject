a78aad70bceb21397beb14448653803d1afa7f0e
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-10-04 20:33:47 -0400

    creating a css component for Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,17 @@
 import React from "react";
-import "./Header.css"
+import "./Header.css";
+import Navbar from "./Navbar";
 
 
-const Header = (props) => {
+const Header = () => {
     return (
         <>
+            <Navbar/>
             <header>
                 <h1>Design Quotes</h1>
                 <br/>
                 <h2>Inspiration Quotes from Great Designers</h2>
                 <br/>
-                <br/>
-                <nav>
-                    <ul>
-          
-                    </ul>
-                </nav>
             </header>
         
         
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+    <nav className='nav'>
+    <a href="" className='site-title'>Business Site</a>
+    <ul>
+        <li><a href="" className='site-title'>Business Site</a></li>
+        <li><a href="" className='site-title'>Business Site</a></li>
+    </ul>
+    </nav>
+    </>
   )
 }
 
-export default Navbar
+export default Navbar;
